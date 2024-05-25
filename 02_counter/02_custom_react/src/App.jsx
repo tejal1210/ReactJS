c3bf77e3dc91f26290ab1e1f4030e7329a30d848
Tejal Shirsat <tejalshirsat94@gmail.com>
2024-05-25 19:14:37 +0530

    Learned React Hooks

---
+++ 02_counter/02_custom_react/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let [count, setCount] = useState(0)
+  const increment= ()=>{
+    setCount(count+1);
+  }
+  const decrement = () => {
+    if(count>0){
+      setCount(count-1)
+    }
+    else{
+      alert("Count cannot be decremented")
+    }
+  }
+  return (
+    <>
+      <h1>Tejal's Practice</h1>
+      <h2>Count Value: {count}</h2>
+      <button onClick={increment}>INCREMENT COUNT : {count}</button>
+      <button onClick={decrement}>DECREMENT COUNT : {count}</button>
+      <p>footer : {count} </p>
+
+
+    </>
+  )
+}
+
+export default App
