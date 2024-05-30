8c1ef1be56a0165290fb35ed90c37c001a6c1d30
Tejal Shirsat <tejalshirsat94@gmail.com>
2024-05-30 17:14:59 +0530

    Built a toggle theme project to learn Context API

---
+++ 09_theme_switcher/09_theme_switcher/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from 'react'
+import { ThemeContextProvider } from './context/ThemeContext'
+import './App.css'
+import ThemeButton from './components/ThemeButton'
+import Card from './components/Card'
+
+function App() {
+  const [themeMode, setThemeMode] = useState('light')
+
+  const lightTheme = () => {
+    setThemeMode('light')
+  }
+  const darkTheme = () => {
+    setThemeMode('dark')
+  }
+
+  useEffect(() => {
+    const classes = document.querySelector('html').classList
+    classes.remove('light', 'dark')
+    classes.add(themeMode)
+  }, [themeMode])
+  
+  return (
+    <>
+      <ThemeContextProvider value={{themeMode, lightTheme, darkTheme}}>
+        <div className="flex flex-wrap min-h-screen items-center">
+          <div className="w-full">
+              <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                  <ThemeButton/>
+              </div>
+
+              <div className="w-full max-w-sm mx-auto">
+                  <Card />
+              </div>
+          </div>
+        </div>
+      </ThemeContextProvider>
+    </>
+  )
+}
+
+export default App
